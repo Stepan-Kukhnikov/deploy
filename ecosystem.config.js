1be0e69a0d8136891ec489c8da3f6ca3a5bf8b26
Кухников Степан <Kukhnikov@lanit.ru>
2026-04-19 15:52:55 +0300

    add ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: '.env.deploy' });
 
 const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
@@ -7,16 +7,12 @@ const {
 module.exports = {
   apps: [{
     name: 'backend',
-    script: './app.js',
-    cwd: '/home/user/deploy/backend',
+    script: './dist/app.js',
+    cwd: `${DEPLOY_PATH}/source/backend`,
     env_production: {
       NODE_ENV: 'production',
-      PORT: 3000
+      PORT: 3000,
     },
-    env_development: {
-      NODE_ENV: 'development',
-      PORT: 3000
-    }
   }],
 
   deploy: {
@@ -26,7 +22,9 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'git@github.com:Stepan-Kukhnikov/deploy.git',
       path: DEPLOY_PATH,
+      'pre-deploy': `scp ./backend/.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend/.env`,
       'post-deploy': 'cd backend && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      key: '~/.ssh/vm_key',
     },
   },
 };

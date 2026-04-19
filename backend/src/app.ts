b36a7a2f4628f5fe7f0726382c5d7e6635daa733
Кухников Степан <Kukhnikov@lanit.ru>
2026-04-19 19:33:23 +0300

    fix: enable CORS for production

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,23 @@
 import 'dotenv/config';
-
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
-// import cors from 'cors';
+import cors from 'cors';
 import errorHandler from './middlewares/error-handler';
 import { DB_ADDRESS } from './config';
 import routes from './routes';
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
 mongoose.connect(DB_ADDRESS);
 
-// Только для локальных тестов. Не используйте это в продакшене
-// app.use(cors())
+app.use(cors({
+  origin: 'https://deploy.nomorepartiessite.ru',
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -22,5 +25,4 @@ app.use(routes);
 app.use(errors());
 app.use(errorHandler);
 
-// eslint-disable-next-line no-console
 app.listen(PORT, () => console.log('ok'));
